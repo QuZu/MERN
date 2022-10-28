6bd3dbe3a0a5d3cfc30a1c97fff928c9d4c7ed82
QuZu <emertkuzu35@gmail.com>
2022-10-28 03:21:33 +0300

    fix server

--- server.js
+++ server.js
@@ -16,6 +16,16 @@ mongoose
 
 //Use routes
 app.use("/api/items",items)
+if (process.env.NODE_ENV === 'production') {
+    // Exprees will serve up production assets
+    app.use(express.static('../client/build'));
+  
+    // Express serve up index.html file if it doesn't recognize route
+    const path = require('path');
+    app.get('*', (req, res) => {
+      res.sendFile(path.resolve(__dirname, '..','client', 'build', 'index.html'));
+    });
+  }
 
 const port=process.env.PORT || 5000;
 app.listen(port,()=>console.log(`Server started on port"${port}`));
